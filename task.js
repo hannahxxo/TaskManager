f7460b5772706f9f1d7d26b03cccc290e44f45f0
hannahxxo <hannahantle@gmail.com>
2021-03-09 08:52:47 -0600

    Created first prompt

---
+++ task.js
@@ -0,0 +1,9 @@
+// Starting message
+prompt(`TASK MANAGER
+
+What would like to do (Please enter one of the options below):
+"TASKS" - Display All Tasks
+"NEW" - Add A New Task
+"REMOVE" - Remove A Task
+"CLOSE" - Close The Task Manager
+`)
